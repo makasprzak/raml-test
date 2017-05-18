81fc008261f0df3d913366449ddc056aa2ff2288
Maciej Kasprzak <github@makasprzak.pl>
2017-05-18 22:43:04 +0200

    executing check - get scenario, happy path

--- src/main/groovy/pl/kasprzak/raml/test/CheckExecutor.groovy
+++ src/main/groovy/pl/kasprzak/raml/test/CheckExecutor.groovy
@@ -1,4 +1,30 @@
 package pl.kasprzak.raml.test
 
+import static com.jayway.restassured.RestAssured.given
+
 class CheckExecutor {
+    private final int port
+
+    CheckExecutor(port) {
+        this.port = port
+    }
+
+    def execute(EndpointCheck check) {
+        def responseBody = (given().config().port(port).
+                when() as MethodExecutor).executeMethod(check.method, check.path)
+                .then().statusCode(check.okStatus)
+                .extract().response().body().prettyPrint()
+        check.validateResponse(responseBody)
+        responseBody
+    }
+
+    trait MethodExecutor {
+        def executeMethod(String method, String path) {
+            switch (method) {
+                case "GET": return get(path)
+                case "POST": return post(path)
+                default: throw new RuntimeException("Illegal http method! ${method}")
+            }
+        }
+    }
 }
---
+++ src/test/groovy/pl/kasprzak/raml/test/CheckExecutorTest.groovy
@@ -0,0 +1,56 @@
+package pl.kasprzak.raml.test
+
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.HttpRequest
+import org.mockserver.model.HttpResponse
+import org.mockserver.socket.PortFactory
+import spock.lang.Specification
+
+import static com.jayway.restassured.RestAssured.given
+import static org.hamcrest.Matchers.hasSize
+import static spock.util.matcher.HamcrestSupport.expect
+
+class CheckExecutorTest extends Specification {
+    def server
+    def static port = PortFactory.findFreePort()
+    private CheckExecutor executor = new CheckExecutor(port)
+
+    def setup() {
+        server = ClientAndServer.startClientAndServer(port)
+    }
+
+    def cleanup() {
+        server.stop()
+    }
+
+    def "should check request"() {
+        given:
+
+        server
+            .when(
+                HttpRequest.request()
+                    .withMethod("GET")
+                    .withPath("/user")
+        )
+        .respond(
+            HttpResponse.response()
+                .withStatusCode(200)
+                .withBody("""{"name":"John Bean"}""")
+                .withHeader("Content-Type", "application/json")
+        )
+        def invocations = new HashSet<>()
+        Closure<List<String>> validationFunction = { String body ->
+            invocations.add(body)
+            return Collections.emptyList()
+        }
+        def check = new EndpointCheck(method: "GET", path: "/user", okStatus: 200, validateResponse: validationFunction)
+
+        when:
+        executor.execute(check)
+
+        then:
+        expect invocations, hasSize(1)
+    }
+
+
+}
